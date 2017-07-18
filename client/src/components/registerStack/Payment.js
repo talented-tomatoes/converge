090369f696c4f3d2ee4863f39e5020d70d6291d5
ryanplaton <ryanplaton@gmail.com>
2017-07-18 10:58:40 -0700

    Add navigation to Payment page

---
+++ client/src/components/registerStack/Payment.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Text, View } from 'react-native';
+
+export default class Payment extends React.Component {
+
+  static navigationOptions = {
+    title: 'Payment Details'
+  };
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View><Text>Hi Payment</Text></View>
+    );
+  }
+}
+
