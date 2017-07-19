1aab66005d7043f750df556247e89cf50cb870c6
ryanplaton <ryanplaton@gmail.com>
2017-07-19 11:59:26 -0700

    Add cloudinary to default config

--- config/default_config.js
+++ config/default_config.js
@@ -2,5 +2,10 @@ module.exports = {
   stripe: {
     pubKey: '<PUBLISHABLE KEY>',
     secKey: '<SECRET KEY>'
-  }
+  },
+  cloudinary: {
+        cloud_name: '<NAME>',
+        api_key: '<API KEY>',
+        api_secret: '<API SECRET>'
+    }
 }
