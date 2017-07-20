180a0198a5a59558a56ce1c1f8a04ebe2f6b28b4
ryanplaton <ryanplaton@gmail.com>
2017-07-20 13:36:18 -0700

    Update conference and speaker components to talk with server for DB data

--- client/src/components/registerStack/SpeakerList.js
+++ client/src/components/registerStack/SpeakerList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { AppRegistry, Image, TouchableOpacity } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import SpeakerListEntry from './SpeakerListEntry.js';
@@ -7,11 +8,26 @@ import { Container, Header, Right, Content, Card, Title, CardItem, ListItem, Thu
 export default class ConferenceListScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      speakers : []
+    }
   }
 
+  componentDidMount() {
+   axios.get(`http://localhost:3000/api/speakers/${this.props.conferenceID}`)
+    .then(response => {
+      this.setState({
+        speakers: response.data
+      });
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
   render() {
 
-    const speakerListItems = this.props.speakers.map((speaker, i) =>
+    const speakerListItems = this.state.speakers.map((speaker, i) =>
       <SpeakerListEntry key={i} speaker={speaker} navigation={this.props.navigation}/>
     );
 
