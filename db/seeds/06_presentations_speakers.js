2b2ae535e8fc2e40e928874564c113ad6b1c7d5b
codepadma <padma.govindarajalu@gmail.com>
2017-07-23 19:39:22 -0700

    Fixing duplicate key issue

--- db/seeds/06_presentations_speakers.js
+++ db/seeds/06_presentations_speakers.js
@@ -4,13 +4,15 @@ exports.seed = function(knex, Promise) {
   return knex('presentations_speakers').del()
     .then(function () {
       // Inserts seed entries
-      return knex('presentations_speakers').insert([
-        {id: 1, presentation_id: 1, speaker_id: 1},
-        {id: 2, presentation_id: 2, speaker_id: 2},
-        {id: 3, presentation_id: 3, speaker_id: 3},
-        {id: 4, presentation_id: 4, speaker_id: 4},
-        {id: 5, presentation_id: 5, speaker_id: 5},
-        {id: 6, presentation_id: 6, speaker_id: 6},
+      return knex('presentations_speakers')
+      .returning('id')
+      .insert([
+        {presentation_id: 1, speaker_id: 1},
+        {presentation_id: 2, speaker_id: 2},
+        {presentation_id: 3, speaker_id: 3},
+        {presentation_id: 4, speaker_id: 4},
+        {presentation_id: 5, speaker_id: 5},
+        {presentation_id: 6, speaker_id: 6}
       ]);
     });
 };
