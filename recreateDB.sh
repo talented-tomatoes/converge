0d2fb2445fe2b026abb139b2e8c816e33f069fd3
Alexander Chang <awchang56@gmail.com>
2017-07-25 11:20:57 -0700

    changes db-start shell script and adds proper googleid to users seed file

--- recreateDB.sh
+++ recreateDB.sh
@@ -1,5 +1,5 @@
-psql -c "drop database converge"
 psql -c "create database converge"
+knex migrate:rollback --knexfile "db/knexfile.js"
 knex migrate:latest --knexfile "db/knexfile.js"
-knex seed:run --knexfile db/knexfile.js
+knex seed:run --knexfile "db/knexfile.js"
 
