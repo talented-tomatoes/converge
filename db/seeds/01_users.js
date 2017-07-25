6ccc19046ada02fb9214bf10f9bb9cfbe16bddab
Alexander Chang <awchang56@gmail.com>
2017-07-25 00:25:06 -0700

    fixes warnings/errors for bash script db-start and fixes navigation bug on eventDetails

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
       .returning('id')
       .insert([
         {login_id: '106873821099349941383', first_name: 'Padma', last_name: 'Govindarajalu', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500820911/Padma_Govindaradalu_1_imqyhp.jpg', email: 'padma.govindarajalu@gmail.com', linkedin_id: 'https://www.linkedin.com/in/padma-govindarajalu-59092ba/', phone_number:'123-234-1234', user_type: 'host' },
-        {login_id: '106873821099349941384', first_name: 'Alex', last_name: 'Chang', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500771623/alex_vshmmf.jpg', email: 'awchang56@gmail.com', linkedin_id: 'https://www.linkedin.com/in/awchang56/', phone_number:'555-555-5555', user_type: 'attendee' },
+        {login_id: '117286496899187654492', first_name: 'Alex', last_name: 'Chang', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500771623/alex_vshmmf.jpg', email: 'awchang56@gmail.com', linkedin_id: 'https://www.linkedin.com/in/awchang56/', phone_number:'555-555-5555', user_type: 'attendee' },
         {login_id: '106873821099349941385', first_name: 'Ryan', last_name: 'Platon', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500822267/Ryan_Platon_fl4nae.jpg', email: 'ryan.platon@gmail.com', linkedin_id: 'https://www.linkedin.com/in/ryanplaton/', phone_number:'444-444-4444', user_type: 'attendee' },
         {login_id: '106873821099349941386', first_name: 'Wilson', last_name: 'Cen', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500822328/Wilson_Cen_cqix5l.jpg', email: 'wilsoncen@gmail.com', linkedin_id: 'https://www.linkedin.com/in/wilsoncen/', phone_number:'666-666-6666', user_type: 'host' }
       ]);
--- recreateDB.sh
+++ recreateDB.sh
@@ -1,4 +1,5 @@
 psql -c "drop database converge"
 psql -c "create database converge"
 knex migrate:latest --knexfile "db/knexfile.js"
+knex seed:run --knexfile db/knexfile.js
 
