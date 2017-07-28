c6bf9cd06ac143a72c360e32602848ee2bb819f7
ryanplaton <ryanplaton@gmail.com>
2017-07-27 21:07:44 -0700

    Have SpeakerDetails component show which presentations the speaker is in

--- client/src/components/registerStack/SpeakerDetails.js
+++ client/src/components/registerStack/SpeakerDetails.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Config from '../../../../config/config.js';
 import { AppRegistry, Text, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
-import { Container, Header, Icon, Content, Title, Body, Card, CardItem, Button, List, ListItem, Thumbnail, Left } from 'native-base';
+import { Container, Header, Icon, Tab, Tabs, Content, Title, Body, Grid, Col, Right, Card, CardItem, Button, List, ListItem, Thumbnail, Left } from 'native-base';
 import RegisterStackHeader from './helpers/RegisterStackHeader.js'
 
 
@@ -8,14 +10,35 @@ export default class SpeakerDetails extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      presentations : []
+    }
   }
 
   static navigationOptions = {
     title: 'Speaker Details'
   };
 
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    axios.get(`${Config.server.url}api/join/presentations_speakers/${params.speaker.id}`)
+      .then(response => {
+        this.setState({
+          presentations: response.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  handleItemPress() {
+
+  }
+
   render() {
     const { params } = this.props.navigation.state;
+    var colors = ['#ff2d55', '#5856d6', '#007aff', '#5ac8fa', '#ffcc22', '#ff954f', '#ff3b30'];
     return (
       <Container>
         <RegisterStackHeader
@@ -34,15 +57,64 @@ export default class SpeakerDetails extends Component {
             </CardItem>
             <CardItem>
               <Body>
-                <Image source={{uri: params.speaker.avatar_url}} style={{height: 200, width: 200, flex: 1}}/>
-                <Text>
+                <Left>
+                  <Image source={{uri: params.speaker.avatar_url}} style={{height: 200, width: 200, flex: 1}}/>
+                </Left>
+                <Text style={{padding: 10}}>
                   {params.speaker.bio}
                 </Text>
               </Body>
             </CardItem>
+            <CardItem>
+              <Body>
+                <Title>Here's where you'll find {params.speaker.first_name + ' ' + params.speaker.last_name}</Title>
+              </Body>
+            </CardItem>
+            <Content>
+              {
+                this.state.presentations.map((presentation, i) => {
+
+                  return (
+                      <List key={i}>
+                        <ListItem avatar onPress={this.handleItemPress.bind(this)}>
+                          <Left>
+                            <Grid style={{ alignSelf: "center", flex: 0}}>
+                              <Col style={{ backgroundColor: colors[Math.floor(Math.random() * (colors.length -1 + 1))], height: 50, width: 5}}></Col>
+                            </Grid>
+                            <Text style={{paddingTop: 15, paddingLeft: 5}}>{presentation.time}</Text>
+                          </Left>
+                          <Body>
+                            <Text>{presentation.name}</Text>
+                            <Text note>{presentation.location}</Text>
+                          </Body>
+                          <Right>
+                            <Text style={{paddingTop: 15}}>{presentation.date}</Text>
+                          </Right>
+                        </ListItem>
+                      </List>
+                  )
+                })
+              }
+            </Content>
           </Card>
         </Content>
       </Container>
     );
   }
 }
+
+/*
+<ListItem onPress={this.handleItemPress.bind(this)} key={i}>
+                            <Left>
+                              <Grid style={{ alignSelf: "center", width: 0, flex: 0, paddingLeft: 5}}>
+                                <Col style={{ backgroundColor:  colors[Math.floor(Math.random() * (colors.length - 1 + 1))], height: 50, width: 5}}></Col>
+                              </Grid>
+                            </Left>
+                            <Body>
+                              <Text>{presentation.name}}</Text>
+                              <Text note>{presentation.location}</Text>
+                            </Body>
+                            <Right>
+                            </Right>
+                        </ListItem>
+*/
