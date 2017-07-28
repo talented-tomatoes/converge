ddb95cdc8427b26ea594f264b5f98a6a86f316dd
ryanplaton <ryanplaton@gmail.com>
2017-07-27 22:22:38 -0700

    Get random color only once. Convert date to english

--- client/src/components/registerStack/SpeakerDetails.js
+++ client/src/components/registerStack/SpeakerDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Config from '../../../../config/config.js';
+import convertDateToEnglish from '../adminStack/helpers/convertDateToEnglish.js';
 import { AppRegistry, Text, TouchableHighlight, TouchableOpacity, Image } from 'react-native';
 import { Container, Header, Icon, Tab, Tabs, Content, Title, Body, Grid, Col, Row, Right, Card, CardItem, Button, List, ListItem, Thumbnail, Left } from 'native-base';
 import RegisterStackHeader from './helpers/RegisterStackHeader.js'
@@ -13,6 +14,8 @@ export default class SpeakerDetails extends Component {
     this.state = {
       presentations : []
     }
+    var colors = ['#ff2d55', '#5856d6', '#007aff', '#5ac8fa', '#ffcc22', '#ff954f', '#ff3b30'];
+    this.randomColor = colors[Math.floor(Math.random() * (colors.length -1 + 1))];
   }
 
   static navigationOptions = {
@@ -38,7 +41,6 @@ export default class SpeakerDetails extends Component {
 
   render() {
     const { params } = this.props.navigation.state;
-    var colors = ['#ff2d55', '#5856d6', '#007aff', '#5ac8fa', '#ffcc22', '#ff954f', '#ff3b30'];
     return (
       <Container>
         <RegisterStackHeader
@@ -48,7 +50,7 @@ export default class SpeakerDetails extends Component {
         />
         <Content style={{padding: 10}}>
           <Card style={{flex: 0}}>
-      
+
             <CardItem>
               <Left>
                 <Body>
@@ -67,7 +69,7 @@ export default class SpeakerDetails extends Component {
               </Body>
             </CardItem>
             <Grid style={{ alignSelf: "center", flex: 0}}>
-              <Col style={{ backgroundColor: colors[Math.floor(Math.random() * (colors.length -1 + 1))], height: 5, flex: 1}}></Col>
+              <Col style={{ backgroundColor: this.randomColor, height: 5, flex: 1}}></Col>
             </Grid>
           </Card>
           <Card>
@@ -85,7 +87,7 @@ export default class SpeakerDetails extends Component {
                         <ListItem avatar onPress={this.handleItemPress.bind(this)}>
                           <Left>
                             <Grid style={{ alignSelf: "center", flex: 0}}>
-                              <Col style={{ backgroundColor: colors[Math.floor(Math.random() * (colors.length -1 + 1))], height: 50, width: 5}}></Col>
+                              <Col style={{ backgroundColor: this.randomColor, height: 50, width: 5}}></Col>
                             </Grid>
                             <Text style={{paddingTop: 15, paddingLeft: 5}}>{presentation.time}</Text>
                           </Left>
@@ -94,7 +96,7 @@ export default class SpeakerDetails extends Component {
                             <Text note>{presentation.location}</Text>
                           </Body>
                           <Right>
-                            <Text style={{paddingTop: 15}}>{presentation.date}</Text>
+                            <Text style={{paddingTop: 15}}>{convertDateToEnglish(presentation.date)}</Text>
                           </Right>
                         </ListItem>
                       </List>
