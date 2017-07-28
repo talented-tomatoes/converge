7558acb8d3a8973b871181815c7a8df1724b2e3e
ryanplaton <ryanplaton@gmail.com>
2017-07-28 16:06:53 -0700

    Style the home page to make it look similar to other styled pages

--- client/src/components/attendeeStack/Home.js
+++ client/src/components/attendeeStack/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Image } from 'react-native';
-import { Drawer, Content, Header, Left, Body, Right, Footer, FooterTab, Icon, Button, Title, Text } from 'native-base';
+import { Drawer, Content, Header, Container, Card, CardItem, Grid, Col, Left, Body, Right, Footer, FooterTab, Icon, Button, Title, Text } from 'native-base';
 import AttendeeConferenceHeader from './helpers/AttendeeConferenceHeader.js'
 import AttendeeConferenceFooter from './helpers/AttendeeConferenceFooter.js';
 import SideBar from './Sidebar';
@@ -9,6 +9,8 @@ import SideBar from './Sidebar';
 class Home extends Component {
   constructor(props) {
     super(props);
+    var colors = ['#ff2d55', '#5856d6', '#007aff', '#5ac8fa', '#ffcc22', '#ff954f', '#ff3b30'];
+    this.randomColor = colors[Math.floor(Math.random() * (colors.length -1 + 1))];
   }
 
   closeDrawer() {
@@ -22,7 +24,49 @@ class Home extends Component {
   render() {
     const { params } = this.props.navigation.state;
     return (
-      <Drawer
+      <Container>
+        <Drawer
+        ref={(ref) => { this.drawer = ref; }}
+        content={<SideBar navigator={this.navigator} navigation={this.props.navigation} />}
+        onClose={() => this.closeDrawer()} >
+          <AttendeeConferenceHeader
+            leftOnPress={this.openDrawer.bind(this)}
+            leftIcon="menu"
+            title="Welcome"
+          />
+          <Content style={{padding: 10}}>
+            <Card style={{flex: 0}}>
+              <Image source={{uri: this.props.conference.banner}} style={{height: 200, flex: 1}}/>
+              <CardItem>
+                <Body>
+                  <Text style={{padding: 10}}>
+                    {this.props.conference.details}
+                  </Text>
+                </Body>
+              </CardItem>
+              <Grid style={{ alignSelf: "center", flex: 0}}>
+                <Col style={{ backgroundColor: this.randomColor, height: 5, flex: 1}}></Col>
+              </Grid>
+            </Card>
+          </Content>
+          <AttendeeConferenceFooter navigation={this.props.navigation}></AttendeeConferenceFooter>
+        </Drawer>
+      </Container>
+    
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    conference: state.attendeeReducer
+  }
+}
+
+export default connect(mapStateToProps)(Home);
+
+/*
+<Drawer
         ref={(ref) => { this.drawer = ref; }}
         content={<SideBar navigator={this.navigator} navigation={this.props.navigation} />}
         onClose={() => this.closeDrawer()} >
@@ -39,14 +83,4 @@ class Home extends Component {
         </Content>
         <AttendeeConferenceFooter navigation={this.props.navigation} />
       </Drawer>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    conference: state.attendeeReducer
-  }
-}
-
-export default connect(mapStateToProps)(Home);
+*/
