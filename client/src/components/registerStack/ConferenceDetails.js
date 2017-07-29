db302df4aa2e2b5669fd993e2ff6952c0ba3bf3f
ryanplaton <ryanplaton@gmail.com>
2017-07-28 21:50:14 -0700

    Add styling to ConferenceDetails component

--- client/src/components/registerStack/ConferenceDetails.js
+++ client/src/components/registerStack/ConferenceDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, TouchableOpacity } from 'react-native';
 import { TabNavigator } from 'react-navigation';
-import { Container, Header, Text, Content, Title, Body, Card, CardItem, Button, List, ListItem, Thumbnail, Left } from 'native-base';
+import { Container, Header, Text, Grid, Col, Content, Title, Body, Card, CardItem, Button, List, ListItem, Thumbnail, Left } from 'native-base';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import SpeakerList from './SpeakerList.js';
@@ -12,6 +12,8 @@ class ConferenceDetails extends Component {
 
   constructor(props) {
     super(props);
+    var colors = ['#ff2d55', '#5856d6', '#007aff', '#5ac8fa', '#ffcc22', '#ff954f', '#ff3b30'];
+    this.randomColor = colors[Math.floor(Math.random() * (colors.length -1 + 1))];
   }
 
   render() {
@@ -39,6 +41,9 @@ class ConferenceDetails extends Component {
                  <Text style={{color: 'white'}}>Attend</Text>
                </Button>
              </CardItem>
+             <Grid style={{ alignSelf: "center", flex: 0}}>
+                <Col style={{ backgroundColor: this.randomColor, height: 5, flex: 1}}></Col>
+              </Grid>
           </Card>
           <SpeakerList navigation={this.props.navigation} conferenceID={this.props.selectedConference.id} backPage={'ConferenceDetails'}/>
          </Content>
