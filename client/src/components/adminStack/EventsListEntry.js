4aeb6d18005a7b2d1b2a0bc9126e2186829611c4
ryanplaton <ryanplaton@gmail.com>
2017-07-30 11:26:04 -0700

    Change dates to english

--- client/src/components/adminStack/EventsListEntry.js
+++ client/src/components/adminStack/EventsListEntry.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import converDateToEnglish from './helpers/convertDateToEnglish';
 import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import { Image } from 'react-native';
 
@@ -8,15 +9,16 @@ export default class EventsListEntry extends Component {
   }
 
   render() {
+    var year = this.props.eventData.start_date.slice(0, 4);
     return (
-      <Content>
+      <Content style={{padding: 10}}>
         <Card>
           <CardItem>
             <Left>
               <Thumbnail source={{uri: this.props.eventData.logo}} />
               <Body>
                 <Text>{this.props.eventData.name}</Text>
-                <Text note> {this.props.eventData.details}</Text>
+                <Text note> {`${converDateToEnglish(this.props.eventData.start_date)}, ${year} to ${converDateToEnglish(this.props.eventData.end_date)}, ${year}`}</Text>
                 </Body>
               </Left>
            </CardItem>
@@ -26,11 +28,6 @@ export default class EventsListEntry extends Component {
               style={{height: 200, width: null, flex: 1}} 
               />
             </CardItem>
-            <CardItem>
-              <Left>
-                <Text> {this.props.eventData.start_date}   -to-   {this.props.eventData.end_date}</Text>
-                </Left>
-                </CardItem>
           </Card>
       </Content>
     );
