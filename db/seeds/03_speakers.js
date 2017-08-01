4486598d8fb399e776a58a988f645a23ce41fd01
cen19 <wilsoncen@gmail.com>
2017-07-31 17:24:48 -0700

    (progress on seeding the database with data)

--- db/seeds/03_speakers.js
+++ db/seeds/03_speakers.js
@@ -13,6 +13,170 @@ exports.seed = function(knex, Promise) {
         {first_name: 'Anna', last_name: 'Fang', job_title: 'CEO', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500833987/Anna_Fang_cm5g0b.png', bio: 'Anna Fang is the CEO and Partner of ZhenFund, an angel investment fund active in China\'s TMT sector. Anna oversees the fund\'s investments, portfolio management and operations. Since starting the fund in 2011 with Xu Xiaoping, Anna has managed the fund’s over 300 early stage investments. She also sits on the Boards of many of ZhenFund’s investments.', email: 'anna.fang@gmail.com', linkedin_id: 'https://www.linkedin.com/in/anna_fang', conference_id: 1},
         {first_name: 'Kristen', last_name: 'Green', job_title: 'Managing Director', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500834316/KristenGreen_i0muen.png', bio: 'Kirsten Green used a unique and unconventional blend of professional history and acquired investment experience to launch San Francisco-based Forerunner Ventures in 2010, where she currently serves as Founder and Managing Director. Noticing that emerging purchasing processes were linear and ripe for improvement, Kirsten developed a pacesetter mentality and analytical eye to remain ahead of experience-driven retail trends and identify compelling brand platforms and visionary entrepreneurs. As a founder, Kirsten has lead efforts to raise over $250M from leading investors and has invested in more than 40 early stage companies. Forerunner Ventures is the only VC firm to invest in both Dollar Shave Club and Jet.com, two of the biggest and highest-profile e-commerce exits in recent years, and counts Birchbox, Bonobos, Glossier, Hotel Tonight, Warby Parker and Zola among its portfolio companies. She currently serves on the Board of Directors of several Forerunner portfolio companies including Glossier, InTurn, Outdoor Voices, Ritual and Rockets of Awesome among others. In an unpredictable consumer landscape, Kirsten’s 20+ years of evaluation and investment success stems from a combination of product savvy, retail acumen and a thesis-driven approach that has been the basis of all Forerunner investments — from pre-revenue startups to multi-billion-dollar enterprises- to date. Prior to Forerunner, Kirsten was an equity research analyst and investor at Banc of America Securities, formerly Montgomery Securities, covering publicly-traded retail and consumer stocks. Kirsten graduated from UCLA with a B.A. in Business Economics, and earned a CPA license and a CFA certification.', email: 'kristen.green@gmail.com', linkedin_id: 'kristengreen', conference_id: 2},
         {first_name: 'Michael', last_name: 'Wayne', job_title: 'Founder and CEO', avatar_url: 'https://res.cloudinary.com/awchang56/image/upload/v1500834578/MichaelWayne_FounderandCEO-KinCommunity__2702_tdj2ca.jpg', bio: 'Wayne founded Kin Community in 2007 and has grown the business into a global digital media company that inspires and entertains women around the world through home-focused content and community. Kin Studios produces world-class original and branded video content in categories that include food, home, and DIY. Kin Community is a network of 120 of the best lifestyle content creators across all social platforms. Kin Community works with the world’s leading advertisers to deliver high-quality online video and social campaigns. Wayne is a founding member of GOVA, the trade association for online video, and served as the founding Chairman of the IAWTV. He has lectured at the S.I. Newhouse School of Public Communication at Syracuse University, USC Annenberg School for Communication & Journalism and UCLA’s Anderson School of Business. He has been featured in numerous publications, including Business Week, The Wall Street Journal and The New York Times. Before founding Kin Community, Wayne served as Vice President of Strategic Alliances for Sony Pictures. Prior to joining Sony, Wayne served as Vice President, Business Development for Launch Media where he was part of the core team that grew the online music media company from start-up through IPO and ultimately to its acquisition by Yahoo! Prior to Launch Media, Wayne was a Marketing Manager for The ABC Television Network. He began his career as an entrepreneur in Prague, Czech Republic, where he founded Velvet Magazine, the first English-language city magazine in Central and Eastern Europe. Wayne earned his Bachelor of Arts in English Literature from The University of the South, Sewanee, TN. He’s a board member of the Los Angeles chapter of Girls Inc., a non-profit organization that provides more than 140,000 girls across the U.S. and Canada with life-changing experiences and solutions to the unique challenges girls face.', email: 'michael.wayne@gmail.com', linkedin_id: 'michaelwayne', conference_id: 2},
+        {
+          first_name: 'Neha',
+          last_name:  'Sampat',
+          job_title: 'CEO',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/sampat-neha.png',
+          bio: 'Neha Sampat (@nehasf) is the CEO of Built.io, a company that brings startup innovation to large organizations. With 15+ years experience in enterprise software, Neha is a recognized industry leader and has led product marketing and online experiences for companies like Sun Microsystems and VMware. She is a proponent of diversity and an outspoken advocate for nurturing women leaders in her industry. Neha sits on the Board for the Application Developers Alliance with a passion for supporting professional growth for developers. Neha can be found appearing on keynote stages or expert panels discussing her experiences as an entrepreneur, a female leader or how Built.io is changing the world through automation. Neha was named a “San Francisco Business Times 40 under 40” honoree and one of “50 Women in Tech Dominating Silicon Valley” in 2015.',
+          email: 'nehasampat@example.com',
+          linkedin_id: 'nehasampat',
+          conference_id: 3
+        },
+        {
+          first_name: 'Michael',
+          last_name: 'Stowe',
+          job_title: 'Developer Relations Manager',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/stowe-michael.png',
+          bio: 'Professional technologist and marketer with over 15 years experience. Author, speaker, and consultant with a focus on community development, software architecture, API design, security, and usability.',
+          email: 'michaelstowe@example.com',
+          linkedin_id: 'mikestowe',
+          conference_id: 3
+        },
+        {
+          first_name: 'Rob',
+          last_name: 'Zazueta',
+          job_title: 'Director of Digital Strategy',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/zazueta-rob.png',
+          bio: 'Crazy passionate about helping enterprises, entrepreneurs, small businesses and startups use technology to drive sales and increase revenues. My career has spanned across business development with an emphasis on technical integrations and channel partnerships, technical sales and support, web application design and programming and online and social marketing. I also spent three years as an independent business owner focused on providing technical consulting for small business customers.',
+          email: 'robzazueta@example.com',
+          linkedin_id: 'techknowme',
+          conference_id: 3
+        }, 
+        {
+          first_name: 'Matt',
+          last_name: 'McLarty',
+          job_title: 'Vice President, API Academy',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/Matt.jpg',
+          bio: `20+ years in Information Technology Enterprise Architecture, Microservices, Distributed Systems, API Management, Mobile/Cloud/Social/Application/IoT/Big Data Integration, DevOps, REST, SOA, Web Services, Middleware, Information Security`,
+          email: 'mattmclarty@example.com',
+          linkedin_id: 'mattmclartybc',
+          conference_id: 3
+        },
+        {
+          first_name: 'Todd',
+          last_name: 'Greene',
+          job_title: 'Founder, CEO of PubNub',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/Todd.jpg',
+          bio: `As co-founder and CEO of PubNub, we've delivered a groundbreaking cloud service that levels the playing field, bringing the power of massively multi-user applications to any developer, at any budget.`,
+          email: 'toddgreene@example.com',
+          linkedin_id: 'toddngreene',
+          conference_id: 3
+        }, 
+        {
+          first_name: 'Tanya',
+          last_name: 'Vlahovic',
+          job_title: 'Senior Software Architect (eBay Inc)',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/AAEAAQAAAAAAAAiRAAAAJDIyNWFlYzBhLWY5MjItNGU1Yi05OWJiLTEwOWRjOGM5ZWY4Mg1.jpg',
+          bio: '15+ years of experience in complete software development life cycle: requirements phase, design and architecture, development, testing, deployment, data analysis, and monitoring. In-depth knowledge of designing, implementing and deploying APIs, user entity resolution, risk assessment and decision making systems. ',
+          email: 'tvlahovic@example.com',
+          linkedin_id: 'tvlahovic',
+          conference_id: 3
+        },
+        {
+          first_name: 'Aldo', 
+          last_name: 'Bucchi',
+          job_title: 'Principal Architect (MuleSoft)',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2014/12/aldoheadshot.png',
+          bio: `Hacker, Salesman, Manager, Coach, Consultant, Engineer. 15 years in the IT industry. Advisor to early stage startups and innovation consultant to CXO types at >1B corporations.`,
+          email: 'abucchi@example.com',
+          linkedin_id: 'aldobucchi',
+          conference_id: 3
+        }, 
+        {
+          first_name: 'Dustin',
+          last_name: 'Whittle',
+          job_title: 'Developer Advocate (Uber)',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/06d1510.jpg',
+          bio: `At the moment I specialize in developing scalable web platforms and developer advocacy. I enjoy contributing to making the web better.`
+          email: 'dwhittle@example.com',
+          linkedin_id: 'dustinwhittle',
+          conference_id: 3
+        },
+        {
+          first_name: 'Chris'
+          last_name: 'Matthieu',
+          job_title: 'Director, IoT Engineering (Citrix Octoblu)',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/AAEAAQAAAAAAAAlfAAAAJDg5ZWZkZmJjLTgxM2UtNDY4YS04MDc4LTA4Nzc0OGI2NjFhOQ.jpg',
+          bio: 'Highly experienced Senior Executive recognized and recruited to drive the development, launch, and market expansion of best in class enterprise software. An expert in developing architecture, SOA, SaaS, and layers of technology from network to web. A proven Business Leader and strategist with a lengthy record of success resolving strategic business issues and creating new market opportunities and disruptive operational models via software. A key player in the evolution of technology and business strategies for companies including BlueCross BlueShield of AZ, Hoosier & Arizona Lotteries, and Accenture with a record of success identifying and leading the introduction of emerging technologies and business models into existing and brand new product portfolios.',
+          email: 'cmatthieu@example.com',
+          linkedin_id: 'chrismatthieu',
+          conference_id: 3
+        },
+        {
+          first_name: 'Mary',
+          last_name: 'Cochran',
+          job_title: 'Middleware Consultant (Red Hat)',
+          avatar_url: 'https://apiworld.co/wp-content/uploads/2016/04/AAEAAQAAAAAAAAYwAAAAJDZlZjE0MzIxLTQxMmUtNGRhMC05ODk2LThmOTEwZTc2Zjc4NA.jpg',
+          bio: 'I am a software consultant for Red Hat. I work mainly in the Middleware area with a strong focus on Integration. I work with clients as well as on internal initiatives. My product focus is on JBoss Fuse and the technologies within it.',
+          email: 'mcochran@example.com',
+          linkedin_id: 'mary-cochran-31201252',
+          conference_id: 3
+        },
+        {
+          first_name: 'Catheryne',
+          last_name: 'Nicholson',
+          job_title: 'CEO (BlockCypher)',
+          avatar_url: 'http://apiworld.co/wp-content/uploads/2016/04/AAEAAQAAAAAAAAiIAAAAJDJhYzExNTgyLTFiZTQtNDkyMy05YTM0LWMyYzNiMjhhNzJiOA.jpg',
+          bio: '15+ years in software product management, product marketing, environmental & energy, and defense; 5+ years as a U.S. Naval Officer.',
+          email: 'cnicholson@example.com',
+          linkedin_id: 'catherynenicholson',
+          conference_id: 3
+        },
+        {
+          first_name: 'Erik',
+          last_name: 'Wilde',
+          job_title: 'Unavailable'
+          avatar_url: 'Unavailable',
+          bio: 'Unavailable',
+          email: 'Unavailable',
+          linkedin_id: 'Unavailable',
+          conference_id: 3
+        },
+        {
+          first_name: 'Christopher'
+          last_name: 'Dow'
+          job_title: 'Vice President, Software, August home, Inc.'
+          avatar_url: 'https://static.sched.com/a4/1674479/avatar.jpg.320x320px.jpg?4d7',
+          bio: `Christopher Dow runs the software organization for August, where he is responsible for front-end, back-end, mobile, and firmware development. 
+
+          Prior to August, Christopher Dow was senior vice president, Architecture at Rovi where he was responsible for directing the architecture of Rovi digital entertainment products and product ecosystem, including the Rovi guide, metadata, search and recommendations solutions. He joined Rovi in 2007, when the company acquired Mediabolic, and has over 22 years in the computer graphics and multimedia industry. Chris has directed the high-level architecture for consumer electronics (CE) devices such as digital video recorders, web pads and hand-held gaming devices. He has also led the development of 3D graphics APIs, the media resource solution of a set-top box in an interactive TV environment, and has experience with imaging and X server development.`,
+          email: 'cdow@example.com',
+          linkedin_id: 'christopherdow',
+          conference_id: 3
+        },
+        {
+          first_name: 'Hunter',
+          last_name: 'Loftis',
+          job_title: 'Engineering Manager, Languages, Heroku',
+          avatar_url: 'https://static.sched.com/a12/4195255/avatar.jpg.320x320px.jpg?816',
+          bio: `Full-stack JavaScript, C#, Unity, VR, Node.js engineer who loves occasionally escaping the keyboard. I manage the Languages team at Heroku and believe that programming should be playful.`,
+          email: 'hloft@example.com'
+          linkedin_id: 'hunterloftus',
+          conference_id: 3
+        },
+        {
+          first_name: 'Christian',
+          last_name: 'Posta',
+          job_title: 'Principal Artchitect (Cloud Native Apps, Red Hat)',
+          avatar_url: `https://static.sched.com/a12/3419237/avatar.jpg.320x320px.jpg?5f3`,
+          bio: `Christian Posta (@christianposta) is a Principal Middleware Specialist/Architect at Red Hat and well known for being an author (Microservices for Java Developers, O’Reilly 2016), frequent blogger, speaker, open-source enthusiast and committer on Apache ActiveMQ, Apache Camel, Fabric8 and others. Christian has spent time at web-scale companies and now helps companies creating and deploying large-scale distributed architectures - many of what are now called Microservices based. He enjoys mentoring, training and leading teams to be successful with distributed systems concepts, microservices, devops, and cloud-native application design.`,
+          email: 'christianposta@example.com'
+          linkedin_id: 'ceposta',
+          conference_id: 3
+        },
       ]);
     });
 };
+
+// template
+        {
+          first_name: 
+          last_name:
+          job_title:
+          avatar_url:
+          bio:
+          email:
+          linkedin_id:
+          conference_id:
+        },
--- db/seeds/04_presentations.js
+++ db/seeds/04_presentations.js
@@ -12,7 +12,100 @@ exports.seed = function(knex, Promise) {
         {name: 'STARTUP ALLEY', description: 'At the heart of Disrupt lies Startup Alley where hundreds of early-stage companies in a variety of verticals showcase their talent and technology to attendees, investors and members of the press. Startup Alley is open to companies that are less than 2 years old with less than $2.5M in funding.', date: '2017-09-23', time: '11:00AM', location: 'Green Room', conference_id: 1},
         {name: 'The Late Late Show with James Corden. Internet Success, TV Reach', description: 'James Corden s post-midnight talk show on CBS is more than just a TV sensation. The producers carefully crafted much of the show so it would translate to internet success as well as appeal to a broadcast audience — to the tune of 1 billion YouTube views, Emmy awards, and spin off series. How did they do it? This session brings together the key team to share their recipe for creating these segments that work so well across both media. In addition, they will explore their creative process, how they learn from what does not work, and how you can apply their lessons to creating your own watercooler moments.', date: '2018-06-20', time: '10:00AM', location: 'Ballroom E', conference_id: 2},
         {name: 'Emerging Business Models: The Changing Face of the MCN', description: 'This session features 1:1 interviews with 4 of the most well-known multi-channel networks, as we explore how their business models have changed to address marketplace changes, the explosion of new platforms, life after acquisition, and more. After the set of 1:1 interviews, we will bring all the execs up for a roundtable discussion on what the future looks like.', date: '2018-06-23', time: '11:00AM', location: 'Ballroom C', conference_id: 2},
-        {name: 'Edutainment', description: 'Remember when you stared at the clock for what seemed like an eternity in Mrs. William s third period Biology class, praying that somehow you could move time and end the most boring lecture possible? If you do — and are convinced that learning CANNOT be entertaining — prepare to be proven wrong! Online video has changed how we learn; find out how from these top-notch EDU creators.', date: '2018-06-22', time: '2:00PM', location: 'Red Room', conference_id: 2}
+        {name: 'Edutainment', description: 'Remember when you stared at the clock for what seemed like an eternity in Mrs. William s third period Biology class, praying that somehow you could move time and end the most boring lecture possible? If you do — and are convinced that learning CANNOT be entertaining — prepare to be proven wrong! Online video has changed how we learn; find out how from these top-notch EDU creators.', date: '2018-06-22', time: '2:00PM', location: 'Red Room', conference_id: 2},
+        {
+          'location': 'Workshop Room 3',
+          'time': '12:00 PM',
+          'id': 14,
+          'date': '2017-09-25',
+          'conference_id': 3,
+          'description': 'For most organizations today, application integration is being solved with “point-to-point” solutions that spiderweb between apps and services. This is simply an extension of the legacy enterprise integration patterns we’ve seen for 2 decades. But with modern enterprise using more than 1000 cloud-based applications, this point-to-point model doesn’t scale and need to change. Enter Data Hubs. By focusing on the data you care about rather than the interfaces and APIs of your applications - your integration architecture is able to scale, while creating a governance and mediation layer. Data and application integration leaders should develop such a strategy to determine effective mediation of semantics, and to identify data sharing requirements across applications, constituencies and ecosystems.\n',
+          'name': 'API Integration with a Data Hub'
+        },
+        {
+          'location': 'Workshop Room 1',
+          'time': '12:00 PM',
+          'id': 15,
+          'date': '2017-09-25',
+          'conference_id': 3,
+          'description': 'Talk about the future of microservices',
+          'name': 'New Frontiers for Microservices'
+        },
+        {
+          'location': 'Workshop Room 2',
+          'time': '12:00 PM',
+          'id': 16,
+          'date': '2017-09-25',
+          'conference_id': 3,
+          'description': 'Tod Greene\'s talk on APIs',
+          'name': 'Featured Talk: PubNub'
+        },
+        {
+          location: 'Workshop Room 1',
+          time: '1:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: 'TBA',
+          name: 'Service Landscaping with a Purpose'
+        },
+        {
+          location: 'Workshop Room 1',
+          time: '1:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: 'TBA',
+          name: 'Optimizing User Experience Using Point-to-Point Real-time messaging'
+        },
+        {
+          location: 'Workshop Room 3',
+          time: '2:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: `Join this session for an introduction on how to build moving experiences with the Uber Developer Platform. Instantly integrate the ability to move people and things from A to B into any app.`,
+          name: 'Building Moving Experiences'
+        },
+        {
+          location: 'Workshop Room 1',
+          time: '2:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: 'TBA',
+          name: 'Panel: the people side of microservices'
+        },
+        {
+          location: 'Workshop Room 2',
+          time: '2:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: `First described by Martin Fowler back in 2010, blue-green deployment is a release technique that reduces downtime and risk by running two identical production environments called Blue and Green. Fast-forwarding to 2013, Danilo Sato from ThoughtWorks published on company's blog a very insightful article that describes how to implement blue-green deployments using AWS. We, at Mitoc Group, are working primarily with serverless computing from AWS, and this talk will share our experience using blue-green deployment process for serverless powered applications.`,
+          name: 'Blue-Green Deployments for Serverless Powered Applications on AWS'
+        },
+        {
+          location: 'Workshop Room 3',
+          time: '3:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: `When we discuss APIs these days, we automatically assume HTTPS-based REST APIs. This is a great standard when working with application integration. Not so great, when working with the Internet of Things. Devices these days are communicating over many protocols. A few of the most popular IoT protocols include: HTTPS, Web Sockets, MQTT, CoAP, XMPP, and AMQP. We have gone the extra mile with our open source Meshblu IoT platform to adapt our HTTPS REST API to each of these protocols natively. This strategy allows devices connected via any means to interact with other connected devices communicating in virtually any protocol using the device's native protocol. It's like a French-to-English-to-German translator for IoT. The API world is changing again with the addition of the Internet of Things. Let's consider these new devices and protocols in our API strategies.`,
+          name: `Don't Stop @ REST`
+        },
+        {
+          location: 'Workshop Room 4',
+          time: '3:00 PM',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: 'TBA',
+          name: 'RabbitMQ Without the Complexity'
+        },
       ]);
     });
 };
+
+
+        {
+          location: 'Workshop Room',
+          time: '',
+          date: '2017-09-25',
+          conference_id: 3,
+          description: '',
+          name: ''
+        },
