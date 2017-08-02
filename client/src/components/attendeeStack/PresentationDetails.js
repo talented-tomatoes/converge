0ba155c6ef6d80918c34d9eef61aceca9d5e0d38
ryanplaton <ryanplaton@gmail.com>
2017-08-02 14:45:47 -0700

    Allow user to add to their schedule from presentation details page

--- client/src/components/attendeeStack/PresentationDetails.js
+++ client/src/components/attendeeStack/PresentationDetails.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import convertDateToEnglish from '../adminStack/helpers/convertDateToEnglish'
 import { connect } from 'react-redux';
 import { TouchableOpacity, ScrollView, View } from 'react-native';
-import { Drawer, Container, Header, Badge, Title, Col, Grid, List, Content, Card, CardItem, ListItem, Thumbnail, Body, Text, Right, Left, Icon, Button } from 'native-base';
+import { Drawer, Container, Header, Toast, Badge, Title, Col, Grid, List, Content, Card, CardItem, ListItem, Thumbnail, Body, Text, Right, Left, Icon, Button } from 'native-base';
 import AttendeeConferenceHeader from './helpers/AttendeeConferenceHeader.js'
 import AttendeeConferenceFooter from './helpers/AttendeeConferenceFooter.js';
 import randomColor from '../helpers/randomColor';
@@ -42,12 +42,41 @@ class PresentationsDetails extends Component {
       })
   }
 
+  handleSavePress(presentation) {
+
+    axios.post(`${Config.server.url}api/join/users_presentations`, { presentation_id: presentation.id, user_id: this.props.user.id })
+      .then(response => {
+        //TODO: Try to store this in redux?
+        if (response.data === 'success') {
+          Toast.show({
+              text: `Added ${presentation.name} to your schedule`,
+              position: 'bottom',
+              buttonText: 'X',
+              type: 'success',
+              duration: 2000
+          });
+        }
+        if (response.data === 'already added') {
+          Toast.show({
+            text: 'Looks like you already added this to your schedule. Please check My Schedule for more details.',
+            position: 'bottom',
+            buttonText: 'X',
+            type: 'warning',
+            duration: 2000
+          })
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   render() {
     const { params } = this.props.navigation.state;
     return (
       <Container>
         <AttendeeConferenceHeader
-          leftOnPress={() => this.props.navigation.navigate(params.backPage)}
+          leftOnPress={() => this.props.navigation.goBack()}
           leftIcon="arrow-back"
           title="Presentation"
         />
@@ -58,6 +87,10 @@ class PresentationsDetails extends Component {
                 <Body>
                 <Text style={{fontWeight: 'bold', padding: 10}}>{params.presentation.name}</Text>
                 </Body>
+                  <Button transparent onPress={this.handleSavePress.bind(this, params.presentation)}>
+                    <Icon name="ios-calendar-outline" style={{fontSize: 35, color: this.randomColor, paddingTop: 10}}></Icon>
+                    <Icon name="ios-add-circle" style={{fontSize: 20, color: this.randomColor, position: 'absolute', left: 29, top: 20}}></Icon>
+                  </Button>
               </CardItem>
               <List style={{paddingRight: 20}}>
                 <ListItem icon>
@@ -99,7 +132,7 @@ class PresentationsDetails extends Component {
                 this.state.speakers.map((speaker, i) => {
                   return (
                     <Card key={i}>
-                      <TouchableOpacity onPress={() => this.props.navigation.navigate('SpeakerDetails', { backPage: 'PresentationDetails', speaker: speaker, data: params.presentation })}>
+                      <TouchableOpacity onPress={() => this.props.navigation.navigate('SpeakerDetails', { backPage: 'PresentationDetails', speaker: speaker, data: params.presentation, isUserPaid: true })}>
                       <CardItem header>
                           <Thumbnail small source={{ uri: speaker.avatar_url || 'https://rentcircles.com/assets/no-pic.jpg' }} />
                           <TouchableOpacity style={{marginLeft: 15}} onPress={() => this.props.navigation.navigate('SpeakerDetails', { backPage: 'PresentationDetails', speaker: speaker, data: params.presentation })}>
@@ -122,7 +155,8 @@ class PresentationsDetails extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    conference: state.attendeeReducer
+    conference: state.attendeeReducer,
+    user: state.userReducer
   }
 }
 
